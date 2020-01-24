d5b4d866328bf1832261f0ae9875de6140eabba1
pdr-clv <pdr.clv@gmail.com>
2020-01-24 17:50:08 +0300

    Updates

---
+++ precache-manifest.e33db6a5e6a12424f4ce6ace52e6c061.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e3f58d1bb7f1fd256622c6bcf97a31",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "c9a04e3aead3fe65d9d8",
+    "url": "/monsters/static/css/main.085af4e2.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c9a04e3aead3fe65d9d8",
+    "url": "/monsters/static/js/main.5df12c40.chunk.js"
+  },
+  {
+    "revision": "475d84c361081acc4a17",
+    "url": "/monsters/static/js/runtime-main.05e5ea3c.js"
+  }
+]);
